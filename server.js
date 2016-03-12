cdc5f130d0d54a6974bab7dd318d884456b3ad04
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-03-12 22:18:36 +0200

    add express configuration

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.MONGO_USER && process.env.MONGO__PASSWORD) {
 connectionUrl.push(process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.DB_NAME);
 connectionUrl = connectionUrl.join('');
 
-let connectionOptions = {
+const connectionOptions = {
   server: {
     auto_reconnect: true,
     socketOptions: {keepAlive: 30000, connectTimeoutMS: 0, socketTimeoutMS: 0}
@@ -61,3 +61,40 @@ function subscribeToMongoEvents(connection) {
     console.error(error.message);
   });
 }
+
+const app = express();
+app.set('port', process.env.SERVER_PORT || 3000);
+app.use(compress(6));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  if (req.path === '/api/upload') {
+    next();
+  } else {
+    lusca.csrf()(req, res, next);
+  }
+});
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+//app.use(function(req, res, next) {
+//  res.locals.user = req.user;
+//  next();
+//});
+//app.use(function(req, res, next) {
+//  if (/api/i.test(req.path)) {
+//    req.session.returnTo = req.path;
+//  }
+//  next();
+//});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
