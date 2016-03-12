0d7055361b82978e4d7d0c506bd3012f4a39951e
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-03-12 21:53:22 +0200

    add mongoose connection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const compress = require('compression');
@@ -17,4 +19,45 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 dotenv.load({ path: '.env' });
 
-console.log(process.env.TEST);
+// connection string format: 'mongodb://username:password@localhost:27017/test';
+let connectionUrl = [];
+connectionUrl.push('mongodb://');
+if (process.env.MONGO_USER && process.env.MONGO__PASSWORD) {
+  connectionUrl.push(process.env.MONGO_USER + ':' + process.env.MONGO__PASSWORD + '@');
+}
+connectionUrl.push(process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.DB_NAME);
+connectionUrl = connectionUrl.join('');
+
+let connectionOptions = {
+  server: {
+    auto_reconnect: true,
+    socketOptions: {keepAlive: 30000, connectTimeoutMS: 0, socketTimeoutMS: 0}
+  }
+};
+
+subscribeToMongoEvents(mongoose.connection);
+mongoose.connect(connectionUrl, connectionOptions);
+
+function subscribeToMongoEvents(connection) {
+  connection.on('connected', () => {
+    console.log('Mongoose connected');
+  });
+  connection.on('open', () => {
+    console.log('Mongoose connection opened');
+  });
+  connection.on('disconnecting', () => {
+    console.log('Mongoose disconnecting');
+  });
+  connection.on('db: disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+  connection.on('close', () => {
+    console.log('Mongoose connection closed');
+  });
+  connection.on('reconnected', () => {
+    console.log('Mongoose reconnected');
+  });
+  connection.on('error', (error) => {
+    console.error(error.message);
+  });
+}
