04324f7478147aa88e86bb13c87c128fd9d831aa
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-03-13 19:33:47 +0200

    fix livereload

--- src/lib/lib.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-//= ../../node_modules/jquery/dist/jquery.min.js
-//= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
-//= ../../node_modules/angular2/bundles/angular2-polyfills.min.js
-//= ../../node_modules/rxjs/bundles/Rx.umd.min.js
-//= ../../node_modules/angular/bundles/angular2-all.umd.min.js
