9d8df0ef64cc0ecfe47adb8b90ecd895a8bf18fe
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-03-19 23:47:25 +0200

    update vendors and typings

--- src/vendors.ts
+++ src/vendors.ts
@@ -1,14 +1,5 @@
 // Polyfills
-import 'es6-shim/es6-shim.min.js';
-//import 'es6-promise/dist/es6-promise.min.js';
-import 'reflect-metadata/Reflect.js';
-
-import 'zone.js/dist/zone-microtask.min.js';
-import 'zone.js/dist/long-stack-trace-zone.min.js';
-
-// Vendors
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'angular2/bundles/angular2-polyfills.js';
 
 // Angular 2
 import 'angular2/platform/browser';
@@ -16,10 +7,9 @@ import 'angular2/core';
 import 'angular2/http';
 import 'angular2/router';
 
-// RxJS 5
-//import 'rxjs/Rx';
-
-
 // For vendors for example jQuery, Lodash, angular2-jwt import them here
 // Also see src/typings.d.ts as you also need to run `typings install x` where `x` is your module
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 import "lodash/lodash.min.js";
