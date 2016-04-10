42ac370f8c6e5a7cfd818cb7df0439ea59bf1817
Arthur Osipenko <arthur.osipenko@gmail.com>
2016-04-10 21:24:13 +0300

    switch on lusca secure middelware

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-//const lusca = require('lusca');
+const lusca = require('lusca');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const session = require('express-session');
 const expressValidator = require('express-validator');
 const multer = require('multer');
 const path = require('path');
@@ -17,7 +17,7 @@ const authController = require('./backend/controller/auth');
 const userController = require('./backend/controller/user');
 const dashboardController = require('./backend/controller/dashboard');
 
-dotenv.load({ path: '.env' });
+dotenv.load({path: '.env'});
 
 require('./backend/db');
 
@@ -29,25 +29,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
 app.use(cookieParser());
-// app.use(function(req, res, next) {
-//   if (req.path === '/api/upload') {
-//     next();
-//   } else {
-//     lusca.csrf()(req, res, next);
-//   }
-// });
-// app.use(lusca.xframe('SAMEORIGIN'));
-// app.use(lusca.xssProtection(true));
-//app.use(function(req, res, next) {
-//  res.locals.user = req.user;
-//  next();
-//});
-//app.use(function(req, res, next) {
-//  if (/api/i.test(req.path)) {
-//    req.session.returnTo = req.path;
-//  }
-//  next();
-//});
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET
+}));
+app.use(function (req, res, next) {
+  if (req.path === '/api/upload') {
+    next();
+  } else {
+    //lusca.csrf()(req, res, next);
+    next();
+  }
+});
+app.use(lusca({
+  csp: {/* ... */},
+  xframe: 'SAMEORIGIN',
+  p3p: 'ABCDEF',
+  hsts: {maxAge: 31536000, includeSubDomains: true, preload: true},
+  xssProtection: true
+}));
+
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: 31557600000}));
 
 app.post('/api/login', userController.postLogin);
@@ -61,17 +63,17 @@ app.use('/api/*', authController.checkToken);
 
 app.get('/api/dashboard', dashboardController.getDashboard);
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.use('*', function(req, res, next) {
+app.use('*', function (req, res, next) {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
@@ -79,7 +81,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log(`Server listening on port ${app.get('port')} in ${app.get('env')} mode`);
 });
 
